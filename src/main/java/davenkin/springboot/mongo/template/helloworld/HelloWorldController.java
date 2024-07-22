1a0a5800cee2ab2d61497e2e16ed0890db0b1bd4
Yun Yun4 Teng <tengyun4@lenovo.com>
2024-07-22 12:09:31 +0800

    init

--- src/main/java/davenkin/springboot/mongo/template/helloworld/HelloWorldController.java
+++ src/main/java/davenkin/springboot/mongo/template/helloworld/HelloWorldController.java
@@ -1,10 +1,10 @@
 package davenkin.springboot.mongo.template.helloworld;
 
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import java.util.Map;
 
 @RestController
 public class HelloWorldController {
